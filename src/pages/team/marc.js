ca739c646d46cf280014457d4b239dc62435cd35
Félix Dorn <felix.dorn.fd@gmail.com>
2019-05-18 22:19:53 +0200

    add profile page logic

---
+++ src/pages/team/marc.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import Layout from "../../components/layout"
+export default class marc extends Component {
+  render() {
+    return (
+      <Layout>
+        <div classNa>
+
+        </div>
+      </Layout>
+    )
+  }
+}
